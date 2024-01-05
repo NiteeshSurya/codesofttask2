a73e8d1972712c781a5cb406f2e98013109b1b6a
NiteeshSurya <145772813+NiteeshSurya@users.noreply.github.com>
2024-01-05 21:51:49 +0530

    Initial commit

--- src/codesofttask2.java
+++ src/codesofttask2.java
@@ -1,2 +1,45 @@
-public class codesofttask2 {
+import java.util.Scanner;
+public class codesofttask2
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + " (out of 100): ");
+            int marks = scanner.nextInt();
+
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid input. Marks should be between 0 and 100. Exiting.");
+                return;
+            }
+
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / numSubjects;
+        char grade = calculateGrade(averagePercentage);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
 }
